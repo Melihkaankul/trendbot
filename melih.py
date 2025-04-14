89d8c9ad32159ebce9657c03dc8fcadc3f77bf34
Melihkaankul <melihkkul@gmail.com>
2025-04-14 21:46:14 +0300

    Add files via upload

---
+++ melih.py
@@ -0,0 +1,80 @@
+import streamlit as st
+import requests
+import xml.etree.ElementTree as et
+import sqlitecloud
+from datetime import datetime
+
+
+def trendgetir(ulke="TR"):
+    r = requests.get(f'https://trends.google.com/trending/rss?geo={ulke}')
+    kod = r.status_code
+    if kod == 200:
+        veri = r.content
+        veri = et.fromstring(veri)
+        trendler = []
+        for i in veri.findall('channel/item'):
+            title = i[0].text
+            trafik = int(i[1].text.replace("+", ""))
+            haberler = []
+            for etiket in i:
+                if "news_item" in etiket.tag:
+                    newsitem = {}
+                    newsitem['title'] = etiket[0].text
+                    newsitem['url'] = etiket[2].text
+                    newsitem['resim'] = etiket[3].text
+                    newsitem['kaynak'] = etiket[4].text
+                    haberler.append(newsitem)
+            trend = {}
+            trend['title'] = title
+            trend['trafik'] = trafik
+            trend['haberler'] = haberler
+            trendler.append(trend)
+
+        return trendler
+    else:
+        return False
+
+
+def trendekle(title, trafik, ):
+    conn = sqlitecloud.connect(
+        "sqlitecloud://cdmzfpcahz.g2.sqlite.cloud:8860/chinook.sqlite?apikey=FpNREZerLXaLfnw0K1TYLdshBSEVlFP1U3erozLh1ac")
+    c = conn.cursor()
+    c.execute("SELECT * FROM trendler WHERE title=?", (title,))
+    say = c.fetchone()
+    if len(c.fetchall()) == 0:
+        şimdi = str(datetime.now())
+        c.execute("INSERT INTO trendler VALUES(?,?,?)", (title, trafik, şimdi))
+        id = c.lastrowid
+        conn.commit()
+        return id
+    else:
+        trend = c.fetchone()
+        return trend[0]
+
+
+def haberekle(trend, title, url, resim, kaynak):
+    conn = sqlitecloud.connect(
+        "sqlitecloud://cdmzfpcahz.g2.sqlite.cloud:8860/chinook.sqlite?apikey=FpNREZerLXaLfnw0K1TYLdshBSEVlFP1U3erozLh1ac")
+    c = conn.cursor()
+    c.execute("SELECT * FROM haberler WHERE url=?", (url,))
+    say = c.fetchall()
+    if len(say) == 0:
+        şimdi = str(datetime.now())
+        tarih = int(datetime.timestamp(datetime.now()))
+        c.execute("INSERT INTO haberler VALUES(?,?,?,?,?,?)", (trend, title, url, resim, kaynak, tarih))
+        id = c.lastrowid
+
+
+conn = sqlitecloud.connect(
+    "sqlitecloud://cdmzfpcahz.g2.sqlite.cloud:8860/chinook.sqlite?apikey=FpNREZerLXaLfnw0K1TYLdshBSEVlFP1U3erozLh1ac")
+c = conn.cursor()
+c.execute("CREATE TABLE IF NOT EXISTS trendler(title TEXT,trafik INTEGER,tarih TEXT)")
+conn.commit()
+c.execute("CREATE TABLE IF NOT EXISTS haberler(trend INTEGER,title TEXT,url TEXT,resim TEXT,kaynak TEXT,tarih INTEGER)")
+conn.commit()
+trendler = trendgetir()
+for t in trendler:
+    id = trendekle(t['title'], t['trafik'])
+    haberler = t['haberler']
+    for h in haberler:
+        haberekle(id, h['title'], h['url'], h['resim'], h['kaynak'])
